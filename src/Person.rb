753cafa25d241621fdc27f093ba2500b6fb7ac5e
Darren Lim <darren.lim@Darrens-MacBook-Pro.local>
2018-09-04 16:12:45 +1000

    New Modules and modified main and Person class

--- src/Person.rb
+++ src/Person.rb
@@ -1,7 +1,7 @@
 class Person
 
     attr_reader :name :age :date_of_birth :biological_age
-    attr_writer :is_smoker :alcoholic :body_type :consume_veggies :consume_fruit :happiness_index :biological_age
+    attr_writer :is_smoker :alcoholic :body_type :consume_veggies :consume_fruit :happiness_index :biological_age :hours_of_sleep
 
     def initialize user_name, user_age, user_dob
         @name = user_name
@@ -13,6 +13,7 @@ class Person
         @consume_veggies = 0 #Yes(-10), No(5)
         @consume_fruit = 0 #Yes(-10), No(5)
         @happiness_index = 0 #Not happy(20), Meh(10), Happy(-10)
+        @hours_of_sleep = 0
         @biological_age = 0
     end
 end
--- src/main.rb
+++ src/main.rb
@@ -1,1 +1,34 @@
-puts 'Hello World'
+puts "Main Menu \n------ Biological Age Calculator ------"
+puts "1. Start \n0.Exit"
+print "What would you like to do?: "
+enter_menu = gets.strip.to_i
+
+while enter_menu == 1
+
+    puts "1. User Profile"
+    puts "2. Answer Survey Questions"
+    puts "3. Calculcate Biological Age"
+    puts "0. Exit"
+    puts "\nPick an option: "
+
+    get_index = gets.strip.to_i
+
+    case get_index
+    when 1
+        #SetUserProfile()
+    when 2
+        #Set questions
+    when 3
+        #CalculateBiologicalAge
+    when 0
+        enter_menu = 0
+
+    else
+        puts "Try again!"
+    end
+
+end
+
+puts "Have a nice day!"
+
+
