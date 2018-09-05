fa0b0cba95a8d2d9d68c85df536e2c39f6b33260
Darren Lim <darren.lim@Darrens-MacBook-Pro.local>
2018-09-05 13:17:14 +1000

    Added terminal-table gem to main menu

--- src/main.rb
+++ src/main.rb
@@ -1,13 +1,23 @@
 require_relative 'SetUserProfile.rb'
 require_relative 'SetQuestions.rb'
 require_relative 'Calculate.rb'
+require 'terminal-table'
 
-puts "Main Menu \n------ Biological Age Calculator ------"
-puts "1. Start \n0.Exit"
-print "What would you like to do?: "
+start_menu_rows = []
+start_menu_rows << ["1.", "Start"]
+start_menu_rows << ["0.", "Exit"]
+
+start_menu_table = Terminal::Table.new :title => "Start Menu", :rows => start_menu_rows
+start_menu_table.style = {:width => 40, :padding_left => 3, :border_x => "=", :border_i => "x"}
+
+
+puts start_menu_table
+
+print "What would you like to do?: \n"
 enter_menu = gets.strip.to_i
 
 user_profile = nil
+list_of_answers = nil
 
 def set_user_questions profile, answers
         profile.age = answers[0]
@@ -21,13 +31,40 @@ def set_user_questions profile, answers
         profile.happiness_index = answers[8]
 end
 
-while enter_menu == 1
+def display_user_profile profile
 
-    puts "1. User Profile"
-    puts "2. Answer Survey Questions"
-    puts "3. Calculcate Biological Age"
-    puts "4. Show User Profile"
-    puts "0. Exit"
+    user_profile_row = []
+    user_profile_row << ["Name.", profile.name]
+    user_profile_row << :separator
+    user_profile_row << ["Age.", profile.age]
+    user_profile_row << :separator
+    user_profile_row << ["Date of Birth", profile.date_of_birth]
+
+    display_profile = Terminal::Table.new :title => "Your Profile", :rows => user_profile_row
+
+    return display_profile
+end
+
+def display_main_menu
+    main_menu_rows = []
+    main_menu_rows << ["1.", "Create user profile"]
+    main_menu_rows << :separator
+    main_menu_rows << ["2.", "Answer survey questions"]
+    main_menu_rows << :separator
+    main_menu_rows << ["3.", "Calculate biological age"]
+    main_menu_rows << :separator
+    main_menu_rows << ["4.", "Show Profile"]
+    main_menu_rows << :separator
+    main_menu_rows << ["0.", "Exit"]
+    main_menu_table = Terminal::Table.new :title => "Main Menu", :rows => main_menu_rows
+
+    return main_menu_table
+end
+
+while enter_menu == 1
+    
+    puts "\n"
+    puts display_main_menu
     print "\nPick an option: "
 
     get_index = gets.strip.to_i
@@ -35,31 +72,51 @@ while enter_menu == 1
     case get_index
     when 1
         #SetUserProfile()
-        user_profile = SetUserProfile::initial_questions
-        puts "Name: #{user_profile.name}, Age: #{user_profile.age}, DOB: #{user_profile.date_of_birth}\n"
+        if user_profile == nil
+            user_profile = SetUserProfile::initial_questions
+            puts "Your user profile has been created. Thanks #{user_profile.name}\n"
+        else
+            puts "You already have a profile #{user_profile.name}\n"
+        end
+        
     when 2
         #Set questions
-        if user_profile == nil
-            puts "You do not have a profile. Make a profile first!"
+        if user_profile == nil 
+            puts "You do not have a profile. Make a profile first!\n"
         else
-            list_of_answers = SetQuestions::start_questions
-            set_user_questions(user_profile, list_of_answers)
+            if list_of_answers != nil
+                puts "You already answered the questions!\nGo and calculate your biological age!\n"
+            else
+                list_of_answers = SetQuestions::start_questions
+                set_user_questions(user_profile, list_of_answers)
+                puts "Thank you for answering the questions!\nYou should calculate your biological age now!\n"
+            end
         end
     when 3
         #Calculate the biological age
-        biological_age = Calculate::set_score(user_profile)
-        if biological_age <= 0
-            puts "Subtract #{biological_age} from your current age.
-            \nYour biological age is #{age + biological_age}!"
+
+        if user_profile == nil
+            puts "You do not have a profile. Make a profile first!\n"
+        elsif list_of_answers == nil
+            puts "You haven't answered the questions yet!"
+
         else
-            puts "Add #{biological_age} to your current age.
-            \nYour biological age is #{age + biological_age}"
+            biological_age = Calculate::set_score(user_profile)
+            if biological_age <= 0
+                puts "Subtract #{biological_age} from your current age.
+                \nYour biological age is #{user_profile.age + biological_age}!\n"
+            else
+                puts "Add #{biological_age} to your current age.
+                \nYour biological age is #{user_profile.age + biological_age}\n"
+            end
         end
+        
     when 4
         if user_profile == nil
-            puts "You do not have a profile. Make a profile first!"
+            puts "You do not have a profile. Make a profile first!\n"
         else
-            puts "Name: #{user_profile.name}, Age: #{user_profile.age}, DOB: #{user_profile.date_of_birth}"
+            puts display_user_profile(user_profile)
+
         end
     when 0
         enter_menu = 0
